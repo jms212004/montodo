626f74495460574f4e8bae62aca47682508d1f13
jms <jms212004@gmail.com>
2022-04-04 09:13:29 +0200

    initialisation

---
+++ src/Controller/Admin/ArticleCrudController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Article;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ArticleCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Article::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('title');
+
+        yield SlugField::new('slug')
+            ->setTargetFieldName('title');
+
+        yield TextEditorField::new('content');
+
+        yield TextareaField::new('featuredText', 'Texte mis en avant');
+
+
+        yield DateTimeField::new('createdAt');
+    }
+
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
